a6a69de62f334f2b5b1675b61a9e2345f6f023e5
SaNiKr <sanikr@outlook.com>
2020-10-17 18:13:33 -0600

    404 and 500

--- index.php
+++ index.php
@@ -4,7 +4,19 @@ if (session_id() == "")
     session_start();
 
 include './views/page.php';
-$page = require './views/search.php';
+require './views/search.php';
+
 $search = new Page(getMeta(), renderBody());
+
+
+if(isset($_GET['404'])) {
+    require './views/404.php';
+    $search = new Page(get404(), render404());
+}
+
+if(isset($_GET['500'])) {
+    require './views/500.php';
+    $search = new Page(get500(), render500());
+}
 
 echo $search->page;
---
+++ views/404.php
@@ -0,0 +1,17 @@
+<?php
+
+function render404()
+{
+    return '<div class="container-fluid text-center">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center mt-5">404, could not find what you were looking for!</h1>
+        </div>
+    </div>
+</div>';
+}
+
+function get404()
+{
+    return 'Error 404';
+}
